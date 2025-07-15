7e8cf096bbda0eaaedad61f2adf07df493db665b
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-07-15 17:46:53 +0530

    Add Programs Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Program from "./components/Programs/Program";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
+      <div className="conatiner">
+        <Program />
+      </div>
     </div>
   );
 };
---
+++ src/components/Programs/Program.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { assets } from "../../assets";
+import "./Programs.css";
+
+const Program = () => {
+  return (
+    <div className="programs">
+      <div className="program">
+        <img src={assets.program1} alt="program1" />
+        <div className="captions">
+          <img src={assets.programIcon1} alt="" />
+          <p>Graduation Degree</p>
+        </div>
+      </div>
+      <div className="program">
+        <img src={assets.program2} alt="program2" />
+        <div className="captions">
+          <img src={assets.programIcon2} alt="" />
+          <p>Master Degree</p>
+        </div>
+      </div>
+      <div className="program">
+        <img src={assets.program3} alt="program3" />
+        <div className="captions">
+          <img src={assets.programIcon3} alt="" />
+          <p>Post Graduation Degree</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Program;
