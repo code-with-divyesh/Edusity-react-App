06a8e0365b32f0a40d3e78a4d8c2fbad049251b2
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-07-16 13:45:59 +0530

    Add About component and styling

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Hero.css";
+import { assets } from "../../assets";
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>
+          our cutting-edge is designed to empower students with the
+          knowledge,skils,and experiences needed to excel in the dynamic field
+          of education
+        </p>
+        <button className="btn">
+          Explore More <img src={assets.darkArrow} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
