0beca93b15eb4e2173c9503dc7870baf670ddba4
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-07-16 13:46:34 +0530

    Add About component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
+import Program from "./components/Programs/Program";
+import Title from "./components/Title/Title";
+import About from "./components/About/About";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
+      <div className="conatiner">
+        <Title title="what we offer" subtitle="our Program" />
+        <Program />
+      </div>
+      <div className="container">
+        <About />
+      </div>
     </div>
   );
 };
