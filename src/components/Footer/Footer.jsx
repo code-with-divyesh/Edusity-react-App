2aff0418c340708f7dc843a444eb6c9b8b271d75
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-07-18 12:41:43 +0530

    Added all sections with smooth scroll ids and Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2025 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Terms of Services</li>
+        <li>Privacy Policy</li>
+        <li>
+          <a href="https://github.com/code-with-divyesh/">Github</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/divyesh-gandhi-635243273/">
+            Linkedin
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
